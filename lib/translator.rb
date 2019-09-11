f2f4416a0d8b5ef7a2c5f96b1333b86341f25405
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-11 02:07:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,10 @@ require "yaml"
 def load_library(file_path)
     emoticons_library = YAML.load_file(file_path)
 
-    new_hash = {}
+    new_hash = {
+        get_meaning: {},
+        get_emoticon: {}
+    }
 end
 
 def get_japanese_emoticon
