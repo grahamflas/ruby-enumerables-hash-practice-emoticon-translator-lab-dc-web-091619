b54d3165bcc432c3ea0aeaa59cfa69564b78f219
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-11 01:47:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,13 @@
-# require modules here
+require 'yaml'
 
 def load_library
-  # code goes here
+    emoticons_library = YAML.load_file('emoticons.yml')
 end
 
 def get_japanese_emoticon
-  # code goes here
+# code goes here
 end
 
 def get_english_meaning
-  # code goes here
+# code goes here
 end
