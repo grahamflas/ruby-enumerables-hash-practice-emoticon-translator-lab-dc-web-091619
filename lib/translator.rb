c995f9d0c6d106f2f21714c63b57f280ca1f2821
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-11 13:24:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,16 +9,21 @@ def load_library(file_path)
     }
 
     emoticons_library.each do |key,value|
-        new_hash[:get_meaning][value[1]] = key
-        new_hash[:get_emoticon][value[0]] = value[1]
+        new_hash[:get_meaning][value[1]] = key #Japanese = string description
+        new_hash[:get_emoticon][value[0]] = value[1] #Western = Japanese
     end
-
+    
     new_hash
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-    load_library(file_path)
+#     load_library(file_path)
 
+#     if new_hash[:get_emoticon].key?(emoticon)
+#         return new_hash[:get_emoticon][emoticon]
+#     else
+#         return "Sorry, that emoticon was not found"
+#     end
 end
 
 def get_english_meaning
