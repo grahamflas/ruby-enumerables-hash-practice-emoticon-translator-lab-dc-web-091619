86bbfa0a1210704e8e35b5c7358ac7733ba2f905
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-11 13:26:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-    load_library(file_path)
+    new_hash = load_library(file_path)
 
     if new_hash[:get_emoticon].key?(emoticon)
         return new_hash[:get_emoticon][emoticon]
